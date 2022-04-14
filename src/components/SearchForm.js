e9237881643a8e38e426e9c91736b035183b7a95
Ash <smashraful1995@gmail.com>
2022-04-14 23:54:47 +0600

    initial commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useGlobalContext } from "../context";
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  return (
+    <section className="section search">
+      <div className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SearchForm;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores soluta
+        cum unde illo maiores quaerat repellendus omnis dolor atque quisquam
+        modi quasi ut, nostrum, perferendis ab! Minima reiciendis rem, unde
+        cupiditate consequuntur incidunt sapiente similique? Est, libero sit
+        voluptates assumenda qui magni, saepe impedit accusamus sed eveniet
+        fugiat earum ipsam!
+      </p>
+    </section>
+  );
+};
+
+export default About;
